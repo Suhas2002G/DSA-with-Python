04d553d1183a685be8474a3ea842b40f933eaa9c
suhas <suhas8838@gmail.com>
2025-06-21 21:37:16 +0530

    count number of digits

--- 1.Extraction_of_Digits/2.count_digits.py
+++ 1.Extraction_of_Digits/2.count_digits.py
@@ -17,6 +17,16 @@ from math import log10
 n=123432
 print(int(log10(n)+1))   #6
 
-n1=12
-print(int(log10(n1)+1))     #2
 
+# NOTE : in method-2 : if n == 0 then log10(n)-->raise errror, that why add error-handling
+# n1=0
+# print(int(log10(n1)+1))     Math Domain Error
+
+
+# final code for method-2 
+from math import log10
+n=0
+if n == 0:
+    print(1)
+else:
+    print(int(log10(n)+1))   #1
