61d1c37ae91680914f570fbdbd46cfe41a10c60b
suhas <suhas8838@gmail.com>
2025-06-22 09:12:01 +0530

    revision

---
+++ demo.py
@@ -0,0 +1,26 @@
+# extract digits
+n = 1234
+while n > 0:
+    digit = n % 10 
+    print(digit)
+    n = n//10
+
+
+# count digits
+n=1234
+count = 0
+while n > 0:
+    count += 1
+    n = n // 10
+
+print(f"Digit Count --> {count}")
+
+
+# reverse a number
+n=3456
+rev = 0
+while n>0:
+    rev = rev*10 + n%10
+    n=n//10
+
+print(f"reverse number --> {rev}")
