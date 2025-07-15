56a643fe22d642fa6b3259affe81f68b603e0e1c
suhas <suhas8838@gmail.com>
2025-07-15 21:21:47 +0530

    even odd

--- 1.Extraction_of_Digits/4.armstrong.py
+++ 1.Extraction_of_Digits/4.armstrong.py
@@ -1,19 +1,21 @@
 
+def check_armstrong(num: int) -> bool:
+    n = num
+    length = 0
+    while n > 0:
+        length += 1
+        n = n // 10
 
-def check_armstrong(num:int)->bool:
-    n=num 
-    len=0
-    while n>0:
-        len+=1
-        n=n//10
+    n = num
+    result = 0
+    while n > 0:
+        digit = n % 10
+        result += digit ** length
+        n = n // 10
 
-    n=num
-    result=0
-    while n>0:
-        result=result+n%10**len 
-        n=n//10 
+    return result == num
 
-    return result == num 
 
 
 print(check_armstrong(153))
+print(check_armstrong(1634))
