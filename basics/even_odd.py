f6df8b1d148b5c263431945983200b7edc0cf44b
suhas <suhas8838@gmail.com>
2025-07-15 21:24:12 +0530

    even odd

--- basics/even_odd.py
+++ basics/even_odd.py
@@ -1,4 +1,12 @@
 
 n = 101 
-
 print('even' if n % 2 == 0 else 'odd')
+
+
+
+
+def check_even_odd(n:int)->str:
+    return 'even' if n%2==0 else 'odd'
+
+result = check_even_odd(2002)
+print(result)
